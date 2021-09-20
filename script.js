d0aca1d26ed4d812a622415cfc27b43f81e55a76
shobhit <shobhitsirohi.dev@gmail.com>
2021-09-20 17:44:57 +0530

    responsive elements added

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-var open = document.getElementById("open");
-var menu = document.getElementById("menu");
-var close = document.getElementById("close");
+// var open = document.getElementById("open");
+// var menu = document.getElementById("menu");
+// var close = document.getElementById("close");
 
-menu.style.display = "hidden";
+// menu.style.display = "none";
 
-open.addEventListener("click", function () {
-  if (menu.style.display === "hidden") {
-    menu.style.display = "block";
-  }
-});
+// open.addEventListener("click", function () {
+//   menu.style.display = "block";
+// });
 
-close.addEventListener("click", function () {
-  if (menu.style.display === "block") {
-    menu.style.display = "hidden";
-  }
+// close.addEventListener("click", function () {
+//   menu.style.display = "none";
+// });
+
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+// add event listeners
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
